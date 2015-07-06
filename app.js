87f712958e63fa904a0eb9f29149db69babcb485
Matt Schick <matt@schickm.com>
2015-07-06 16:27:13 -0500

    clean up configuraton to just use environment variables

--- app.js
+++ app.js
@@ -1,17 +1,10 @@
 'use strict';
 
-// try to load the config
-var config;
-try {
-	config = require('./config');
-} catch (e) {
-	config = {
+var config = {
 		secret: process.env.HUB_SECRET,
 		mongo: process.env.HUB_MONGO,
 		port: process.env.PORT
-	};
-
-}
+};
 
 var hub = require('./src/hub').createHub(config);
     
