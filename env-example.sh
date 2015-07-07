36de7164e329db64f8c9ad70203a7dcf0f51cbea
Matt Schick <matt@schickm.com>
2015-07-07 09:00:54 -0500

    quote the env exports to escape dollar signs

--- env-example.sh
+++ env-example.sh
@@ -2,8 +2,8 @@
 
 # BEGIN EDITS
 # kittens in bcrypt
-export HUB_SECRET=$2a$10$vt2TBymKZTKxMz/Z8J6g5OgtG2IslI8A2tGiEO0jYlfZ1XlAxTOsG
-export HUB_MONGO=mongodb://127.0.0.1:27017/mediahubdev 
+export HUB_SECRET='$2a$10$vt2TBymKZTKxMz/Z8J6g5OgtG2IslI8A2tGiEO0jYlfZ1XlAxTOsG'
+export HUB_MONGO='mongodb://127.0.0.1:27017/mediahubdev'
 export PORT=3000
 # END EDITS
  
