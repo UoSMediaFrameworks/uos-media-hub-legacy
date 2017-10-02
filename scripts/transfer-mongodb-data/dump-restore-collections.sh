22279dae03fe463193c060db96717e23147e1984
AAA48574 <aaronp261@gmail.com>
2017-10-02 15:37:37 +0100

    Updated the transfer-mongodb-data script to include another collection.
    - Included abstracting some duplicate code into a basic function.

--- scripts/transfer-mongodb-data/dump-restore-collections.sh
+++ scripts/transfer-mongodb-data/dump-restore-collections.sh
@@ -4,49 +4,42 @@ COLLECTION='mediaScenes'
 COLLECTION_QUERY_FILE='./scripts/transfer-mongodb-data/media-scenes-query.json'
 BACKUP_DIR='./latest-dump'
 
-BACKUP_DIR_FOR_RESTORE=$BACKUP_DIR
-BACKUP_DIR_FOR_RESTORE+='/'
-BACKUP_DIR_FOR_RESTORE+=$EXPORTING_DB
-BACKUP_DIR_FOR_RESTORE+='/'
-BACKUP_DIR_FOR_RESTORE+=$COLLECTION
-BACKUP_DIR_FOR_RESTORE+='.bson'
+function setdir {
+    BACKUP_DIR_FOR_RESTORE=$BACKUP_DIR
+    BACKUP_DIR_FOR_RESTORE+='/'
+    BACKUP_DIR_FOR_RESTORE+=$EXPORTING_DB
+    BACKUP_DIR_FOR_RESTORE+='/'
+    BACKUP_DIR_FOR_RESTORE+=$COLLECTION
+    BACKUP_DIR_FOR_RESTORE+='.bson'
+}
 
+setdir
 # APEP TODO Use Params for mongo path
 # --query $QUERY
 # --collection $COLLECTION
-/c/Program\ Files/MongoDB/Server/3.2/bin/mongodump.exe -h $EXPORTING_MONGO_HOST_PORT -u $EXPORTING_USERNAME -p $EXPORTING_PASSWORD --db $EXPORTING_DB --collection $COLLECTION --queryFile $COLLECTION_QUERY_FILE --out $BACKUP_DIR
 # -u $RESTORING_USERNAME -p $RESTORING_PASSWORD
+/c/Program\ Files/MongoDB/Server/3.2/bin/mongodump.exe -h $EXPORTING_MONGO_HOST_PORT -u $EXPORTING_USERNAME -p $EXPORTING_PASSWORD --db $EXPORTING_DB --collection $COLLECTION --queryFile $COLLECTION_QUERY_FILE --out $BACKUP_DIR
+/c/Program\ Files/MongoDB/Server/3.2/bin/mongorestore.exe --host $RESTORING_MONGO_HOST_PORT --db $RESTORING_DB --collection $COLLECTION $BACKUP_DIR_FOR_RESTORE
+
+COLLECTION='mediaSceneGraphs'
+setdir
+/c/Program\ Files/MongoDB/Server/3.2/bin/mongodump.exe -h $EXPORTING_MONGO_HOST_PORT -u $EXPORTING_USERNAME -p $EXPORTING_PASSWORD --db $EXPORTING_DB --collection $COLLECTION --out $BACKUP_DIR
 /c/Program\ Files/MongoDB/Server/3.2/bin/mongorestore.exe --host $RESTORING_MONGO_HOST_PORT --db $RESTORING_DB --collection $COLLECTION $BACKUP_DIR_FOR_RESTORE
 
 COLLECTION='audiomediaobjects'
-BACKUP_DIR_FOR_RESTORE=$BACKUP_DIR
-BACKUP_DIR_FOR_RESTORE+='/'
-BACKUP_DIR_FOR_RESTORE+=$EXPORTING_DB
-BACKUP_DIR_FOR_RESTORE+='/'
-BACKUP_DIR_FOR_RESTORE+=$COLLECTION
-BACKUP_DIR_FOR_RESTORE+='.bson'
+setdir
 COLLECTION_QUERY_FILE='./scripts/transfer-mongodb-data/amos-query.json'
 /c/Program\ Files/MongoDB/Server/3.2/bin/mongodump.exe -h $EXPORTING_MONGO_HOST_PORT -u $EXPORTING_USERNAME -p $EXPORTING_PASSWORD --db $EXPORTING_DB --collection $COLLECTION --queryFile $COLLECTION_QUERY_FILE --out $BACKUP_DIR
 /c/Program\ Files/MongoDB/Server/3.2/bin/mongorestore.exe --host $RESTORING_MONGO_HOST_PORT --db $RESTORING_DB --collection $COLLECTION $BACKUP_DIR_FOR_RESTORE
 
 COLLECTION='imagemediaobjects'
-BACKUP_DIR_FOR_RESTORE=$BACKUP_DIR
-BACKUP_DIR_FOR_RESTORE+='/'
-BACKUP_DIR_FOR_RESTORE+=$EXPORTING_DB
-BACKUP_DIR_FOR_RESTORE+='/'
-BACKUP_DIR_FOR_RESTORE+=$COLLECTION
-BACKUP_DIR_FOR_RESTORE+='.bson'
+setdir
 COLLECTION_QUERY_FILE='./scripts/transfer-mongodb-data/imos-query.json'
 /c/Program\ Files/MongoDB/Server/3.2/bin/mongodump.exe -h $EXPORTING_MONGO_HOST_PORT -u $EXPORTING_USERNAME -p $EXPORTING_PASSWORD --db $EXPORTING_DB --collection $COLLECTION --queryFile $COLLECTION_QUERY_FILE --out $BACKUP_DIR
 /c/Program\ Files/MongoDB/Server/3.2/bin/mongorestore.exe --host $RESTORING_MONGO_HOST_PORT --db $RESTORING_DB --collection $COLLECTION $BACKUP_DIR_FOR_RESTORE
 
 COLLECTION='videomediaobjects'
-BACKUP_DIR_FOR_RESTORE=$BACKUP_DIR
-BACKUP_DIR_FOR_RESTORE+='/'
-BACKUP_DIR_FOR_RESTORE+=$EXPORTING_DB
-BACKUP_DIR_FOR_RESTORE+='/'
-BACKUP_DIR_FOR_RESTORE+=$COLLECTION
-BACKUP_DIR_FOR_RESTORE+='.bson'
+setdir
 COLLECTION_QUERY_FILE='./scripts/transfer-mongodb-data/vmos-query.json'
 /c/Program\ Files/MongoDB/Server/3.2/bin/mongodump.exe -h $EXPORTING_MONGO_HOST_PORT -u $EXPORTING_USERNAME -p $EXPORTING_PASSWORD --db $EXPORTING_DB --collection $COLLECTION --queryFile $COLLECTION_QUERY_FILE --out $BACKUP_DIR
 /c/Program\ Files/MongoDB/Server/3.2/bin/mongorestore.exe --host $RESTORING_MONGO_HOST_PORT --db $RESTORING_DB --collection $COLLECTION $BACKUP_DIR_FOR_RESTORE
