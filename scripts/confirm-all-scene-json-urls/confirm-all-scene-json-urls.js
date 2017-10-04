0d0b6ff28bc9c61fb2cc84b8f1f202cf109e53a8
Aaron Phillips <aaronp261@gmail.com>
2017-10-04 14:00:04 +0100

    Updated the confirmation script - Distinguish between errors and missing.

--- scripts/confirm-all-scene-json-urls/confirm-all-scene-json-urls.js
+++ scripts/confirm-all-scene-json-urls/confirm-all-scene-json-urls.js
@@ -9,7 +9,6 @@ var path = require("path");
 var sha1 = require('sha1');
 var recursive = require("recursive-readdir");
 var request = require('request');
-var urlExists = require('url-exists');
 
 var config = {
     mongo: process.env.HUB_MONGO,
@@ -19,6 +18,13 @@ var db = mongo(config.mongo, ['mediaScenes']);
 
 var gdcGroups = [ 101, 102, 103, 104, 105, 106, 107, 108, 109, 110];
 var narmGroups = [112];
+
+function urlExists(url, cb) {
+    request({ url: url, method: 'HEAD' }, function(err, res) {
+        if (err) return cb(err, false);
+        cb(null, /4\d\d/.test(res.statusCode) === false);
+    });
+}
 
 db.mediaScenes.find({"_groupID": { "$in": gdcGroups}}, function(err, scenes) {
 
@@ -37,7 +43,11 @@ db.mediaScenes.find({"_groupID": { "$in": gdcGroups}}, function(err, scenes) {
         var externalMediaRemaining = _.filter(allMediaAssets, function(media){
             return media.url.indexOf('mediaframework:8090') === -1;
         });
-        externalMedia.push(externalMediaRemaining);
+
+        // APEP rather than a list of lists, lets just push the single mos
+        _.forEach(externalMediaRemaining, function(mo){
+            externalMedia.push(mo);
+        });
 
         var internalMediaRemaining =_.filter(allMediaAssets, function(media){
             return media.url.indexOf('mediaframework:8090') !== -1;
@@ -45,7 +55,15 @@ db.mediaScenes.find({"_groupID": { "$in": gdcGroups}}, function(err, scenes) {
 
         async.every(internalMediaRemaining, function(mob, cb) {
             urlExists(mob.url, function(err, exists) {
-                if(err || !exists) {
+                if(err) {
+                    errorUrls.push({
+                        sceneId: scene._id,
+                        imoId: mob._id,
+                        url: mob.url,
+                        type: "ERROR",
+                        err: err
+                    });
+                } else if (!exists) {
                     errorUrls.push({
                         sceneId: scene._id,
                         imoId: mob._id,
